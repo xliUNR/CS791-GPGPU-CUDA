7dd1329bb87e446ee73674b572eecab193a83e80
Eric Li <Ericli@Erics-MacBook-Air.local>
2018-01-29 23:02:32 -0800

    changed structure of add

--- PA1/src/hellocuda.cu
+++ PA1/src/hellocuda.cu
@@ -120,7 +120,7 @@ int main() {
     different piece of data, and many threads can execute at the same
     time. This is how cuda can get such large speedups.
    */
-  add<<<grid, block>>>(dev_a, dev_b, dev_c);
+  //add<<<grid, block>>>(dev_a, dev_b, dev_c);
 
   /*
     Unfortunately, the GPU is to some extent a black box. In order to
