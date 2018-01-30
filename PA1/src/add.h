fed2289441cea544c2a685f306abf84834f9f02c
Eric Li <Ericli@Erics-MacBook-Air.local>
2018-01-29 19:48:40 -0800

    took out init function

--- PA1/src/add.h
+++ PA1/src/add.h
@@ -12,6 +12,6 @@
 // This is the declaration of the function that will execute on the GPU.
 __global__ void add(int*, int*, int*);
 
-__global__ void mat_init( int N, int* );
+//__global__ void mat_init( int N, int* );
 
 #endif // ADD_H_
