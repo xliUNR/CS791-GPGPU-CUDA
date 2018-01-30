47ddb0e0bc8b8aa3ef38c4e4c8507b5f1ac23a16
Eric Li <Ericli@Erics-MacBook-Air.local>
2018-01-29 23:36:15 -0800

    added user input for N

--- PA1/src/hellocuda.cu
+++ PA1/src/hellocuda.cu
@@ -133,7 +133,7 @@ for (int i = 0; i < N; i++) {
     different piece of data, and many threads can execute at the same
     time. This is how cuda can get such large speedups.
    */
-  add<<<grid, block>>>(dev_a, dev_b, dev_c);
+  add<<<grid, block>>>(N, dev_a, dev_b, dev_c);
 
   /*
     Unfortunately, the GPU is to some extent a black box. In order to
