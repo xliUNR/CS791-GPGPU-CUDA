dd5dc3a37213b65a8c0b20e80fa9f8d98a25b300
Eric Li <Ericli@Erics-MacBook-Air.local>
2018-01-29 22:14:17 -0800

    added in for loop to fill matrices

--- PA1/src/add.h
+++ PA1/src/add.h
@@ -7,7 +7,7 @@
 #define ADD_H_
 
 // This is the number of elements we want to process.
-#define N 10
+#define N 2
 
 // This is the declaration of the function that will execute on the GPU.
 __global__ void add(int*, int*, int*);
