59be20c74028ed0c558c95283d27450a0b15c8f1
Eric Li <Ericli@Erics-MacBook-Air.local>
2018-01-29 19:06:52 -0800

    fixed arraypop.cu bugs

--- PA1test/src/arraypop.cu
+++ PA1test/src/arraypop.cu
@@ -16,23 +16,12 @@ static void HandleError( cudaError_t err,
 
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
 
+__global__ void add(int *a, int *b, int *c);
+
 __global__ void matpop(int N, int* );
 
 
-//populates a matrix on device
-__global__ void matpop( int N, int *emptyMatrix ){
-  
-  int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
 
-  //check for valid memory location, then initialize element to 0
-  if( thread_id < N * N )
-  {
-    //commented out one is for array of pointers
-    //*((*(emptyMatrix)) + (blockId.x * blockDim.x + threadIdx.x)) = 0;
-    *(emptyMatrix + (blockIdx.x * blockDim.x + threadIdx.x)) = 0;
-  }
-  
-}
 
 
 
@@ -74,7 +63,44 @@ int main(int argc, char const *argv[])
 
 //////////////////// function declarations ////////////////////////////
 
+//populates a matrix on device
+__global__ void matpop( int N, int *emptyMatrix ){
+  
+  int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
 
+  //check for valid memory location, then initialize element to 0
+  if( thread_id < N * N )
+  {
+    //commented out one is for array of pointers
+    //*((*(emptyMatrix)) + (blockId.x * blockDim.x + threadIdx.x)) = 0;
+    *(emptyMatrix + (blockIdx.x * blockDim.x + threadIdx.x)) = 0;
+  }
+  
+}
 
+__global__ void add(int *a, int *b, int *c) {
 
+  /*
+    Each thread knows its identity in the system. This identity is
+    made available in code via indices blockIdx and threadIdx. We
+    write blockIdx.x because block indices are multidimensional. In
+    this case, we have linear arrays of data, so we only need one
+    dimension. If this doesn't make sense, don't worry - the important
+    thing is that the first step in the function is converting the
+    thread's indentity into an index into the data.
+   */
+  int thread_id = blockIdx.x;
+
+  /*
+    We make sure that the thread_id isn't too large, and then we
+    assign c = a + b using the index we calculated above.
+
+    The big picture is that each thread is responsible for adding one
+    element from a and one element from b. Each thread is able to run
+    in parallel, so we get speedup.
+   */
+  if (thread_id < N) {
+    c[thread_id] = a[thread_id] + b[thread_id];
+  }
+}
 
