eb80a3635d5ddcb91de212fd98500f3b0197483e
Eric <eric.li592@gmail.com>
2018-02-08 20:23:18 -0800

    finished correct striding, and finished reduction algo, updated comments, haven't compiled yet v1

--- PA2/src/add.cu
+++ PA2/src/add.cu
@@ -10,106 +10,92 @@
   is not indicated by the function's signature.
  */
 __global__ void add(int n, int *a, int *b, int *c) {
-/*
-  initialize variables, indexes for two arrays being multiplied along with 
-  a cache size variable
-*/
-  int cacheSize, aIndex, bIndex, threadId, blockId, cacheIndex;
-  cacheIndex = threadIdx.x;
+  //initialize variables used for shared memory cache. set Index to 
+  int cacheSize, cacheIndex;
 
-/*
+  //Index of matrix a and matrix b used for element by element multiplication 
+  int aIndex, bIndex;
+  
+  //initialize number of threads for reduction step in multiplication 
+  int reduceThreads;
+
+  /*
+    Calculate the starting block index (x,y). This is used for striding 
+    across grid
+  */
+  int b_x, b_y;
+  b_x = blockIdx.x;
+  b_y = blockIdx.y;
+  
+  /*
   check for even threads per block. Reduction requires even number of threads
   so must pad if odd.
-*/  
-  if( blockDim.x % 2 != 0){
-    cacheSize = blockDim.x + 1;
+  */  
+  if( n % 2 != 0 ){
+    cacheSize = n + 1;
   }
   else{
-    cacheSize = blockDim.x;
+    cacheSize = n;
   }
 
+  //initialize shared cache to store partial results from element by element mult.
   __shared__ cache[cacheSize];
 
-  //pad w/ 0 if odd cacheSize
+  //pad last element of cache with 0 if cacheSize is odd
   if( blockDim.x % 2 != 0 ){
     cache[cacheSize] = 0;
   }
-/* 
-  Loop over n for each thread in a block, One thread corresponds to one element by
-  element multiplication. Results are stored in shared memory cache. This way
-  all blocks will have independent cache. Requires a synchronization step before
-  summing cache for each block. This is to ensure device has finished multiplication 
-  step.  
-*/
-  while( cacheIndex < n ){
-    aIndex = threadIdx.x + blockIdx.y * n ;
-    bIndex = blockIdx.x + threadIdx.x * n ;
-    
-    cache[cacheIndex] = *( a + aIndex ) * ( b + bIndex );
-    
-    //stride to next set of elements
-    cacheIndex+=blockDim.x;
 
- }
- __syncthreads();
+  //Stride loop. I used for because it seemed safer than while loop.  
+  for( int i = (blockIdx.y * gridDim.x + blockIdx.x) ; 
+                              i < n*n; i += gridDim.x * gridDim.y ){
 
+   //Stride loop for threads.
+   for( int i = threadIdx.x; i < n; i+=blockDim.x){
+      //initialize unique indices of input matrix a and b elements
+      aIndex = threadIdx.x + b_y * n;
+      bIndex = b_x + threadIdx.x * n;
 
-/*
-  Use reduction to sum values, when it is done, cache[0] will contain sum for each block
-  which corresponds to a element in the product matrix.
-  
-*/
-int reduceIndex = cacheSize / 2 ;
-while( reduceIndex != 0 ){
-  if(threadIdx.x < reduceIndex ){
-    cache[threadIdx.x] += cache[ threadIdx.x + reduceIndex ];
-  }
-  __syncthreads();
-  reduceIndex /= 2;
-}
+      //calculate index of cache for each thread
+      cacheIndex = threadIdx.x;
 
-//load all values into final matrix c
-*(c + (blockIdx.x + blockIdx.y * n) ) = cache[0];
+      //multiply element by element and store in shared cache.
+      cache[cacheIndex] = ( *( a + aIndex ) ) * ( *(b + bIndex ) );
+      
+      //stride over number of threads
+      cacheIndex+=blockDim.x;
+   }
 
-//striding, in cases were N > number of threads    
-while( thread_id < n*n ){    
-    //c[thread_id] = a[thread_id] + b[thread_id];
-    *(c + thread_id) = *(a + thread_id ) * *(b + thread_id);  
-    //stride to next grid
-    thread_id += blockDim.x * gridDim.x;     
+    /*
+      have to synchronize all threads to make sure cache values are all updated
+      before accessing them for the summation step.
+    */ 
+    __syncthreads(); 
+
+    //calculate Index 
+    reduceThreads = cacheSize / 2 ;
+
+    /*
+      Reduction loop: will loop until summation complete in cache. Need to sync 
+      threads before every addition to avoid race conditions
+    */
+    while( reduceThreads > 0 ){
+    if( threadIdx.x < reduceThreads ){
+      cache[threadIdx.x] += cache[ threadIdx.x + reduceThreads ];
+    }
+    __syncthreads();
+    reduceThreads /= 2;
+    }
+
+    //write results of matrix back to product matrix 
+    *(c + b_y * n + b_x) = cache[0];
+
+    /*Increment block x and y indices. This is required b/c of striding. In order
+      to multiply the correct elements, the indices are incremented independent 
+      of the actual CUDA block indices because those no longer provide the "true"
+      indices once striding is used.
+    */ 
+    b_x+1;
+    b_y++1; 
   }
 }     
-
-
-//matrix add function that uses grid-striding
-__global__ void strideAdd(int n, int *a, int *b, int *c) {
-  //initialize offset AKA unique thread id
-   int option = 0;
-   int thread_id = 0;
-
-   switch (option ) {
-    case 0: 
-       thread_id = threadIdx.x + blockIdx.x * blockDim.x;
-       break;
-
-    case 1:
-       thread_id =  blockIdx.x * blockDim.x * blockDim.y
-                          + threadIdx.y * blockDim.x + threadIdx.x;
-       break;
-       
-    case 2:
-       int blockId = blockIdx.y * gridDim.x + blockIdx.x;  
-       thread_id = blockId * blockDim.x + threadIdx.x;
-       break;                     
-
-    }
-
-
-  //loop over each grid
-  for( int i = thread_id; i < n*n; i+= blockDim.x * gridDim.x )
-    {
-      *(c + thread_id) = *(a + thread_id ) * *(b + thread_id);      
-    }
-
-}
-
