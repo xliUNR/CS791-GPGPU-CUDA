974b934f43cbc98a209586349d9fea9b963e0963
Eric <eric.li592@gmail.com>
2018-02-20 08:28:00 -0800

    finished kernel for distance, now need to compete kernel for k nearest

--- PA3/src/knn.cu
+++ PA3/src/knn.cu
@@ -11,30 +11,53 @@
 
 __global__ void kNN( int *inputMat, int *partialMat, int imputIndex, int rows, 
                                                                      int cols){
-   int blockId, threadId, partialIndex;
+   //initialize variables
+   int blockId, threadId, partialIndex, reduceThreads, reduceIndex;
 
    blockId = blockIdx.x;
-   threadId = threadIdx.x;
+   threadId = threadIdx.x + 2;
+   reduceThreads = cols / 2;
+
    //calculate offset for empty region
    Emptyoffset = ( blockIdx.x * blockDim.x + 2 )*sizeof(int);
    EmptyoffsetIndex = ( blockIdx.x * blockDim.x + 2 );
    
+
    while( blockId < rows )
       {  
          /*
            test to see if block ( time ) has an empty, if it is empty then threads must idle.
          */
-         if( (*inputMat + offset) > 0 ){
+         if( (*inputMat + Emptyoffset) > 0 ){
             while( threadId < cols )
                {  
-                  partIndex = blockId * rows + ;
+                  partIndex = blockId * rows + threadId;
                   partialMat[partialIndex] = 
                          square(inputMat[imputIndex] - inputMat[partIndex]);
                   //stride to next
                   threadId = threadId + blockDim.x;
                }
-            //sync threads b4 reduction   
-            //do reduction summation   
+         //sync threads b4 reduction 
+         __syncthreads();         
+         //do reduction summation   
+         while( reduceThreads > 0 )
+            {             
+               //reset threadId back from striding above
+               threadId = threadIdx.x + 2;
+               while( threadId < reduceThreads )
+                  {  
+                     /*
+                       caclulate index of partial matrix that the reduction 
+                       results are stored in, then sum and stride to next col
+                     */  
+                     reduceIndex = blockId * rows + threadId;
+                     partialMat[ reduceIndex ] += 
+                                  partialMat[ reduceIndex + reduceThreads];
+                     threadId+=blockDim.x;             
+                  }
+               __syncthreads();
+               reduceThreads /= 2;   
+            }
 
          }
         
@@ -42,6 +65,23 @@ __global__ void kNN( int *inputMat, int *partialMat, int imputIndex, int rows,
          blockId = blockId + gridDim.x;   
       }
 
+
+//This function performs reduction to find 5 minimum on the input matrix
+//Need to do parallel bubble sort, then sum first 5 values and average      
+__global__ void reduceMin( int* inMat){
+   int reduceThreads, reduceIndex;
+   reduceIndex = blockIdx.x * cols + 2;
+
+   reduceThreads = rows / 2;
+
+   while( reduceThreads > 5 )
+      {
+         atomicMin(&(inMat[reduceIndex]), 
+                              inputMat[ reduceIndex + reduceThreads]);
+         __syncthreads();
+         reduceThreads /= 2;
+      }
+}  
 
 ///////////////////////////////////////////////////////////////////////////////
 ////////////////////////// helper functions ///////////////////////////////////
