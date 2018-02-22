b2f1ab26b939972cd8b03cb13019e6c9e4ac84aa
Eric <eric.li592@gmail.com>
2018-02-22 02:58:22 -0800

    changed buffer memory alloc to char* instead of float*

--- PA3/src/hellocuda.cu
+++ PA3/src/hellocuda.cu
@@ -43,7 +43,7 @@ int main(int argc, char const *argv[])
    HANDLE_ERROR( cudaMallocManaged( &sortArray, rows*sizeof(float)) );
    
    //allocate memory for read buffer
-   buffer = (float*) malloc(rows*sizeof(float));
+   buffer = (char*) malloc(rows*sizeof(char));
    //open file and read in data
    fp = fopen("PA3_nrdc_data.csv", "r");
    fgets(buffer, rows*sizeof(float), fp);
