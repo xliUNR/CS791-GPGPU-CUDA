06b8a73b00907576660d7f1faeda6e9dfa7c199c
Eric Li <Ericli@Erics-MacBook-Air.local>
2018-02-22 11:54:08 -0800

    making empty cols

--- PA3/src/hellocuda.cu
+++ PA3/src/hellocuda.cu
@@ -57,7 +57,7 @@ int main(int argc, char const *argv[])
          //read in first value, discard and put index i instead as the first column
          getdelim(&charBuffer, &len, " ," ,fp);
          str = strtok( charBuffer, ",");
-         inData[ i*cols ] = (float)i;
+         //inData[ i*cols ] = (float)i;
 
          //loop over all columns and input value into 1D array
          for(int j = 1; j < cols; j++){
