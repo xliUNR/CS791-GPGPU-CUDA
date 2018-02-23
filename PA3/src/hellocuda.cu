659e7515880813d2c3d6c9b37ad3e308300af3d6
Eric <eric.li592@gmail.com>
2018-02-23 01:34:23 -0800

    fixed bug on making holes portion

--- PA3/src/hellocuda.cu
+++ PA3/src/hellocuda.cu
@@ -98,7 +98,7 @@ int main(int argc, char const *argv[])
    numEmpty = (rows <= 10) ? 1: (rows/10);
 
    for(int i = 0; i < numEmpty; i++){
-       inData[ i*rows+1] = -1;
+       inData[ i*cols+1] = -1;
    }   
   //////////////////////////////////////////////////////////////////////////
   //////////////////// sequential Implementation  //////////////////////////
