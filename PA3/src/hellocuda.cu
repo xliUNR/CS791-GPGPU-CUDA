5980231a40e38d6a2c806faa8ae487adb764a30a
Eric Li <Ericli@Erics-MacBook-Air.local>
2018-02-23 22:07:56 -0800

    trying Grid: 32 | block: 32

--- PA3/src/hellocuda.cu
+++ PA3/src/hellocuda.cu
@@ -68,7 +68,7 @@ int main(int argc, char const *argv[])
    }
 
    //declare grid structure
-   dim3 grid(16);
+   dim3 grid(32);
    //dim3 block((cols+32/32));
 
    //allocate Unified memory for input data storage
@@ -202,7 +202,7 @@ int main(int argc, char const *argv[])
         distances stored in the second col of each row. This value still needs to be
         square rooted to get the distance. 
       */  
-      knnDist<<<grid,64>>>(inData, partial, i, rows, readCols, paddedCols);
+      knnDist<<<grid,32>>>(inData, partial, i, rows, readCols, paddedCols);
       //error checking for kernel call
       HANDLE_ERROR( cudaPeekAtLastError() );
       HANDLE_ERROR( cudaDeviceSynchronize() );
@@ -211,7 +211,7 @@ int main(int argc, char const *argv[])
         this kernel squares results stored in col 2 of partial and transfers distance 
         into 1D array for sorting on CPU
       */  
-      distXfer<<<1,64>>>(partial, GPUsortArr, rows, paddedCols);
+      distXfer<<<1,32>>>(partial, GPUsortArr, rows, paddedCols);
       //error checking for kernel call
       HANDLE_ERROR( cudaPeekAtLastError() );
       HANDLE_ERROR( cudaDeviceSynchronize() );
