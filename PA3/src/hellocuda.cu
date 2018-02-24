028e56a3d184cde5329ef4dbaaf27f0af11037d8
Eric Li <Ericli@Erics-MacBook-Air.local>
2018-02-23 16:02:15 -0800

    re added print

--- PA3/src/hellocuda.cu
+++ PA3/src/hellocuda.cu
@@ -180,7 +180,7 @@ int main(int argc, char const *argv[])
         distances stored in the second col of each row. This value still needs to be
         square rooted to get the distance. 
       */  
-      knnDist<<<grid,32>>>(inData, partial, i, rows, cols);
+      knnDist<<<grid,2>>>(inData, partial, i, rows, cols);
       //error checking for kernel call
       HANDLE_ERROR( cudaPeekAtLastError() );
       HANDLE_ERROR( cudaDeviceSynchronize() );
@@ -189,7 +189,7 @@ int main(int argc, char const *argv[])
         this kernel squares results stored in col 2 of partial and transfers distance 
         into 1D array for sorting on CPU
       */  
-      distXfer<<<grid,32>>>(partial, GPUsortArr, rows, cols);
+      distXfer<<<grid,2>>>(partial, GPUsortArr, rows, cols);
       //error checking for kernel call
       HANDLE_ERROR( cudaPeekAtLastError() );
       HANDLE_ERROR( cudaDeviceSynchronize() );
